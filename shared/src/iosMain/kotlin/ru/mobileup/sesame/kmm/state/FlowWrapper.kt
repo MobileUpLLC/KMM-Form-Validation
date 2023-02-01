350016ea9cc5268877ff712e8a054a309ab97565
Takexito <Takexito@gmail.com>
2023-02-01 12:49:03 +0300

    add mutable flow wrapper

--- shared/src/iosMain/kotlin/ru/mobileup/sesame/kmm/state/FlowWrapper.kt
+++ shared/src/iosMain/kotlin/ru/mobileup/sesame/kmm/state/FlowWrapper.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
@@ -11,17 +12,27 @@ interface Cancelable {
     fun cancel()
 }
 
-class FlowWrapper<T : Any>(
+open class FlowWrapper<T : Any>(
     private val flow: Flow<T>
 ) : Flow<T> by flow {
 
     fun bind(consumer: (T) -> Unit): Cancelable {
         val job = Job()
-        onEach { consumer(it) }.launchIn(CoroutineScope(Dispatchers.Main + job))
+        val scope = CoroutineScope(Dispatchers.Main + job)
+        onEach { consumer(it) }.launchIn(scope)
         return object : Cancelable {
             override fun cancel() {
                 job.cancel()
             }
         }
+    }
+}
+
+class MutableStateWrapper<T : Any>(
+    private val stateFlow: MutableStateFlow<T>
+) : FlowWrapper<T>(stateFlow) {
+
+    fun update(value: T){
+        stateFlow.value = value
     }
 }
