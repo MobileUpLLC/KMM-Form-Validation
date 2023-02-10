aa00591f309574594b38ef9c10593632b9be0af8
Nick <80983073+Kotliamba@users.noreply.github.com>
2023-02-10 16:17:10 +0300

    udate ui logic

--- sample/iosSample/iosSample/View/FormView.swift
+++ sample/iosSample/iosSample/View/FormView.swift
@@ -27,81 +27,14 @@ struct FormView: View {
     var body: some View {
         ScrollView(showsIndicators: false) {
             ScrollViewReader { proxy in
-                VStack{
-                    HStack {
-                        Text("Default Form")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .foregroundColor(.gray)
-                        Spacer()
-                    }
-                    .padding(.vertical, 20)
-                    TextFieldWithControl(
-                        inputControl: formComponent.nameInput,
-                        hint: MR.strings().name_hint.desc().localized(),
-                        isSecure: false
-                    )
-                    .scrolledFocus(
-                        focus: _focus,
-                        id: Field.name.rawValue,
-                        nextId: Field.email.rawValue,
-                        proxy: proxy
-                    )
-                    .id(Field.name.rawValue)
-                    TextFieldWithControl(
-                        inputControl: formComponent.emailInput,
-                        hint: MR.strings().email_hint.desc().localized(),
-                        isSecure: false
-                    )
-                    .scrolledFocus(
-                        focus: _focus,
-                        id: Field.email.rawValue,
-                        nextId: Field.phone.rawValue,
-                        proxy: proxy
-                    )
-                    .id(Field.email.rawValue)
-                    TextFieldWithControl(
-                        inputControl: formComponent.phoneInput,
-                        hint: MR.strings().phone_hint.desc().localized(),
-                        isSecure: false
-                    )
-                    .scrolledFocus(
-                        focus: _focus,
-                        id: Field.phone.rawValue,
-                        nextId: Field.password.rawValue,
-                        proxy: proxy
-                    )
-                    .id(Field.phone.rawValue)
-                    TextFieldWithControl(
-                        inputControl: formComponent.passwordInput,
-                        hint: MR.strings().password_hint.desc().localized(),
-                        isSecure: true
-                    )
-                    .scrolledFocus(
-                        focus: _focus,
-                        id: Field.password.rawValue,
-                        nextId: Field.passwordConfirmation.rawValue,
-                        proxy: proxy
-                    )
-                    .id(Field.password.rawValue)
-                    TextFieldWithControl(
-                        inputControl: formComponent.confirmPasswordInput,
-                        hint: MR.strings().confirm_password_hint.desc().localized(),
-                        isSecure: true
-                    )
-                    .scrolledFocus(
-                        focus: _focus,
-                        id: Field.passwordConfirmation.rawValue,
-                        nextId: 5,
-                        proxy: proxy
-                    )
-                    .id(Field.passwordConfirmation.rawValue)
+                VStack {
+                    TitleView()
+                    getTextFiledStackView(proxy: proxy)
                     ToggleView(
                         checkControl: formComponent.termsCheckBox,
                         label: MR.strings().terms_hint.desc().localized()
                     )
                     .id(5)
-                    
                     SubmitButton(
                         label: MR.strings().submit_button.desc().localized(),
                         buttonState: submitButtonState.value!,
@@ -120,8 +53,85 @@ struct FormView: View {
             }
         }
     }
+    
+    private func getTextFiledStackView(proxy: ScrollViewProxy) -> some View {
+        VStack {
+            TextFieldWithControl(
+                inputControl: formComponent.nameInput,
+                hint: MR.strings().name_hint.desc().localized(),
+                isSecure: false
+            )
+            .scrolledFocus(
+                focus: _focus,
+                id: Field.name.rawValue,
+                nextId: Field.email.rawValue,
+                proxy: proxy
+            )
+            .id(Field.name.rawValue)
+            TextFieldWithControl(
+                inputControl: formComponent.emailInput,
+                hint: MR.strings().email_hint.desc().localized(),
+                isSecure: false
+            )
+            .scrolledFocus(
+                focus: _focus,
+                id: Field.email.rawValue,
+                nextId: Field.phone.rawValue,
+                proxy: proxy
+            )
+            .id(Field.email.rawValue)
+            TextFieldWithControl(
+                inputControl: formComponent.phoneInput,
+                hint: MR.strings().phone_hint.desc().localized(),
+                isSecure: false
+            )
+            .scrolledFocus(
+                focus: _focus,
+                id: Field.phone.rawValue,
+                nextId: Field.password.rawValue,
+                proxy: proxy
+            )
+            .id(Field.phone.rawValue)
+            TextFieldWithControl(
+                inputControl: formComponent.passwordInput,
+                hint: MR.strings().password_hint.desc().localized(),
+                isSecure: true
+            )
+            .scrolledFocus(
+                focus: _focus,
+                id: Field.password.rawValue,
+                nextId: Field.passwordConfirmation.rawValue,
+                proxy: proxy
+            )
+            .id(Field.password.rawValue)
+            TextFieldWithControl(
+                inputControl: formComponent.confirmPasswordInput,
+                hint: MR.strings().confirm_password_hint.desc().localized(),
+                isSecure: true
+            )
+            .scrolledFocus(
+                focus: _focus,
+                id: Field.passwordConfirmation.rawValue,
+                nextId: 5,
+                proxy: proxy
+            )
+            .id(Field.passwordConfirmation.rawValue)
+        }
+    }
 }
 
+private struct TitleView: View {
+    var body: some View {
+        HStack {
+            Text("Default Form")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.gray)
+            Spacer()
+        }
+        .padding(.vertical, 20)
+    }
+}
 
 struct FormView_Previews: PreviewProvider {
     static var previews: some View {
